317ad5e9b363df08089c0e570c43845f97baa6d3
Jakob Alexander <jeilinghoff@googlemail.com>
2023-05-31 16:46:12 +0200

    new challenge

---
+++ js-functions-2_basic-calculations/js-functions-2_greeting-page/js/index.js
@@ -0,0 +1,63 @@
+console.clear();
+
+/*
+Change the contents of this page depending on the current day and time.
+
+- Write a function getGreeting that returns a different greeting depending on the current time:
+  - 6 - 12: returns "Good Morning"
+  - 13 - 18: returns "Good Afternoon"
+  - 19 - 22: returns "Good Evening"
+  - 23 - 5: returns "Good Night"
+
+(HINT: you can get the current time: new Date().getHours() )
+
+- Write a function getDayColor that returns a different color depending on the current weekday:
+  - monday: "darkgray"
+  - tuesday - friday: "lightblue"
+  - saturday - sunday: "hotpink"
+
+(HINT: you can get the current weekday: new Date().getDay() )
+
+*/
+
+const display = document.querySelector('[data-js="display"]');
+
+function getGreeting() {
+  // Code here
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+  console.log(currentDate);
+  console.log(currentHour);
+
+  if (currentHour > 6 && currentHour < 12) {
+    return "Good Morning";
+  } else if (currentHour > 13 && currentHour < 18) {
+    return "Good Afternoon"
+  } else if (currentHour > 19 && currentHour < 22) 
+  {return "Good Evening" }
+  
+    else {
+      return "Good Night"
+    }
+  }
+
+
+
+
+function getDayColor() {
+  // Code here
+const currentDate = new Date();
+const currentWeekday = currentDate.getDay();
+
+if (currentWeekday === "Monday") {
+  return "darkgray";
+} else if (currentWeekday == "Tuesday" || currentWeekday ==  "Wednesday" || currentWeekday == "Thursday" || currentWeekday == "Friday") {
+  return "lightblue";
+}
+else {
+  return "hotpink";
+}
+}
+
+display.textContent = getGreeting();
+document.body.style.backgroundColor = getDayColor();
