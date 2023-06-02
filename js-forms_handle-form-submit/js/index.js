fe2258878dc228b872909287aae4d9cc0424e584
Jakob Alexander <jeilinghoff@googlemail.com>
2023-06-02 16:35:57 +0200

    added bonus task

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,6 +1,8 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+let inputAge = document.querySelector("#age")
+let inputBadness = document.querySelector("#badness")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -9,4 +11,15 @@ form.addEventListener("submit", (event) => {
 
     console.log(data);
 
+    function ageBadnessSum() {
+        let age = inputAge.value;
+        console.log(age);
+        let badness = inputBadness.value;
+        console.log(badness);
+
+        let badnessAge = parseInt(age) + parseInt(badness);
+        console.log(badnessAge);
+        return badnessAge
+    }
+    ageBadnessSum();
 })
