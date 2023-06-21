730a337088e98392248bed1cdaba80526f2304b7
Jakob Alexander <jeilinghoff@googlemail.com>
2023-06-21 19:26:43 +0200

    recap 3

--- fs-linting/index.js
+++ fs-linting/index.js
@@ -1,15 +1,32 @@
-function toggleButton() {
-  const cards = document.querySelectorAll('[data-js=card]');
-  const button = card.querySelectorAll('[data-js=buttons]');
-  cards.forEach(card => {
-    const button = card.querySelector('[data-js="button"]');
-    const link = card.querySelector('[data-js="link"]');
-    const arrow = card.querySelector('[data-js="arrow"]');
-    button.addEventListener('click', () => {
-      link.classList.toggle('hide');
-      arrow.classList.toggle('rotate');
-    });
-  });
+// function toggleButton() {
+//   const cards = document.querySelectorAll('[data-js=card]');
+//   const buttons = cards.querySelectorAll('[data-js=buttons]');
+//   cards.forEach(card => {
+//     const button = card.querySelector('[data-js="button"]');
+//     const link = cards.querySelector('[data-js="link"]');
+//     const arrow = card.querySelector('[data-js="arrow"]');
+//     button.addEventListener('click', () => {
+//       link.classList.toggle('hide');
+//       arrow.classList.toggle('rotate');
+//     });
+//   });
+// }
+
+// toggleButton();
+
+const meet = {
+  'tim':0,
+   'jim':2, 
+   'randy':0,
+    'sandy':7, 'andy':0,
+     'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}
+
+
+const boss = {'Laura': 3,
+'jonny': 5};
+
+function outed(meet, boss) {
+console.log(Object.meet.values);
 }
 
-toggleButton();
+outed(meet, boss);
